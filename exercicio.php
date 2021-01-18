eeaa4d3a4b8b5e7d811d084b1652dd5d46ca3d54
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-01-17 23:13:54 -0300

    Initial commit

---
+++ exercicio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos\css\estilo.css">
+    <link rel="stylesheet" href="recursos\css\exercicio.css">
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercícios</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+        
+        </div>
+    </main>
+    <footer class="rodape">
+        Reginaldo Viana DEV @<?= date('Y');?>
+    </footer>
+</body>
+</html>
