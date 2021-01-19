877bb1cf7f0e1216a291750807c262964e2451cb
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-01-19 12:14:04 -0300

    update

--- basico/comentarios.php
+++ basico/comentarios.php
@@ -1,1 +1,23 @@
 <div class="titulo">Comentários PHP</div>
+
+<?php
+echo "Estou no PHP";
+//echo "Comentário";
+# echo "Comentário";
+?>
+
+<p>Depois do primeiro bloco!</p>
+<!--
+     <p>Comentário HTML</p>!
+     Interpretado pelo Browser!
+-->
+
+<?php
+/*
+Linha 1 do comentário
+Linha 2 do comentário
+linha 3 do comentário
+*/
+?>
+
+<p>Depois do segundo bloco!</p>
