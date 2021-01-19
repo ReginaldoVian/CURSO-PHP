a13da5e0e9e049968a3c9bceea7a33f04358ca88
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-01-19 10:43:40 -0300

    update

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
                     <h3>Módulo 01</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
                     </ul>
                 </div>
             </nav>
