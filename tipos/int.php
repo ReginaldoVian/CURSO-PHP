1912c7bd947dddca3b2c51c496406785493a6033
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-01-19 21:40:03 -0300

    update

---
+++ tipos/int.php
@@ -0,0 +1,14 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11, '<br>';
+
+var_dump(11);
+
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 017, '<br>'; // Base octal
+echo 0b0101, '<br>'; // Base binária
+echo 0X11abc; //Base Hexa
