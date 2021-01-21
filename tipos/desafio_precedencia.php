e0cb2bfd5c0f358518f8ef435f1225d86159b706
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-01-21 19:53:08 -0300

    Update desafio_precedencia.php

--- tipos/desafio_precedencia.php
+++ tipos/desafio_precedencia.php
@@ -3,9 +3,8 @@
 <?php
 echo '<p>1. Qual o valor será impresso? </p>';
 echo 2 + 5 * 3 + (12 / 6) / (-8 + 2 ** 3) ;
-// 2 + 5 * 3 + 2 / 16
-// 2 + 15 + 0.125
-//Resultado: 17.125
+//Resultado: INF
+
 
 echo '<p>2. Qual expressão imprime o valor 100?</>p';
 echo ' a) ', (1 + 2) * 20 - 15;
