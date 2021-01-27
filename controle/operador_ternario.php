34904f1b9f544fec27182f9dcaec1dce13a5cf91
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-01-27 20:30:34 -0300

    Update operador_ternario.php

--- controle/operador_ternario.php
+++ controle/operador_ternario.php
@@ -15,3 +15,7 @@ echo "$status<br>";
 $idade = 17;
 $status = $idade >= 18? 'Maior de idade' : 'Menor de idade';
 echo "$status<br>";
+
+$idade = 70;
+$status = $idade >= 18 ? $idade >= 65 ? 'Aposentado' : 'Maior de idade' : 'Menor de idade';
+echo "$status<br>";
