9e26f5da0d5de39f3dc1eea81faf83e58efe8b61
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-01-27 21:17:00 -0300

    Update switch.php

--- controle/switch.php
+++ controle/switch.php
@@ -22,4 +22,31 @@ if($categoria === 'Luxo'){
 $precoFormatado = number_format($preco, 2, ',', '.');
 echo "<p>Carro: $carro<br>Preço: R$ $precoFormatado</p>";
 
-$categoria = '???';
+$categoria = 'luxo';
+switch (strtolower($categoria)){
+    case 'luxo':{
+        $carro = 'Mercedes';
+        $preco = 250000;
+        break;
+    }
+    case 'suv':
+    case 'suv Básico':{
+        $carro = 'Renegate';
+        $preco = 80000;
+        break;
+    }
+    case 'sedan':{
+        $carro = 'Etios';
+        $preco = 55000;
+        break;
+    }
+    default:{
+        $carro = 'Mobi';
+        $preco = 33000;
+        break;
+    }
+
+}
+
+$precoFormatado = number_format($preco, 2, ',', '.');
+echo "<p>Carro: $carro<br>Preço: R$ $precoFormatado</p>";
