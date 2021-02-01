e66d9f00b35e7e8a1d37576b1a05a25431135ded
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-02-01 11:24:15 -0300

    Update for.php

--- repeticoes/for.php
+++ repeticoes/for.php
@@ -18,6 +18,26 @@ for(; $cont <= 15; ){
     $cont++;
 }
 
-$array = [1 => 'Domingp', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+$array = [1 => 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
 
 print_r($array);
+
+echo '<hr>';
+
+for($i = 1; $i <= count ($array); $i++){
+    echo "{$array[$i]} <br>";
+}
+
+$matrix = [
+        ['a', 'e', 'i', 'o', 'u'],
+        ['b', 'c', 'd']
+];
+
+echo "<hr>";
+
+for($i = 0; $i < count($matrix); $i++){
+    for($j = 0; $j < count($matrix[$i]); $j++){
+        echo "{$matrix[$i][$j]} ";
+    }
+    echo "<br>";
+}
