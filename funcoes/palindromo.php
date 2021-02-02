31bd2316963bf6b85a08e109a7301e3e065aaa86
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-02-02 13:57:21 -0300

    Update

---
+++ funcoes/palindromo.php
@@ -0,0 +1,51 @@
+<div class="titulo">Desafio Palindromo</div>
+
+<form action="#" method="post">
+    <div>
+        <label for="caixa">Informe o nome:</label>
+        <input type="text" name="caixa">
+    </div>
+    <button>Verificar</button>
+</form>
+
+
+<?php 
+// Whith loop for
+function palindrome($word){
+    $lastindex = strlen($word) - 1;
+    for($i = 0; $i <= $lastindex; $i++){
+        if($word[$i] !== $word[$lastindex - $i]){
+            return 'Not Palindrome';
+        }
+    }
+    return 'Yes, Palindrome';
+}
+
+echo palindrome($_POST['caixa']) . '<br>';
+
+
+//Using If and Else
+// PHP code to check for Palindrome string in PHP 
+// Using strrev() 
+
+/*$caixa = ($_POST['caixa']);
+
+function Palindrome($string){   
+    if (strrev($string) == $string){   
+        return 1;   
+    } 
+    else{ 
+        return 0; 
+    } 
+}   
+  
+// Driver Code 
+$original = $caixa;  
+if(Palindrome($original)){   
+    echo "Palindrome";   
+}  
+else {   
+echo "Not a Palindrome";   
+} */
+
+?>   
--- index.php
+++ index.php
@@ -14,6 +14,14 @@
     <main class="principal">
         <div class="conteudo">
             <nav class="modulos">
+            <div class="modulo vermelho-escuro">
+                    <h3>7. Funções</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=funcoes&file=escopo">Funções & Escopo</a></li>
+                        <li><a href="exercicio.php?dir=funcoes&file=args_retorno">Args & Retorno</a></li>
+                        <li><a href="exercicio.php?dir=funcoes&file=palindromo">Desafio Palindromo</a></li>
+                    </ul>
+                </div>
             <div class="modulo verde-escuro">
                     <h3>6. Repetições</h3>
                     <ul>
