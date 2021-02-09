6e45cfcc5703af35e7352810090aa0d8128c5585
ReginaldoVian <reginaldo.dearaujo@hotmail.com>
2021-02-09 12:23:41 -0300

    Update desafio_erros.php

--- classes_objetos/desafio_erros.php
+++ classes_objetos/desafio_erros.php
@@ -1,1 +1,29 @@
 <div class="titulo">Desafio Erros</div>
+
+<?php
+interface Template {
+    function metodo1();
+    public function metodo2($parametro);
+}
+
+abstract class ClasseAbstrata implements Template {
+    public function metodo3(){
+        echo "Estou funcionando";
+    }
+
+    public function metodo1(){
+        
+    }
+}
+
+class Classe extends ClasseAbstrata {
+    function __construct($parametro)    {
+    }
+
+    public function metodo2($parametro){
+        
+    }
+}
+
+$exemplo = New Classe('...');
+$exemplo->metodo3();
